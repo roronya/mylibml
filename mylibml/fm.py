872f5a18debeb3146d3a17d6e3e13b6b3049bd1b
roronya <yukikanai0204@gmail.com>
2017-02-06 15:13:48 +0900

    正則化項の付け方変えた

--- mylibml/fm.py
+++ mylibml/fm.py
@@ -50,9 +50,9 @@ class FactorizationMachines(BaseEstimator, RegressorMixin):
         fit_start = time.time()
         saturation_counter = 0
         N, D = X.shape
-        w0 = np.random.rand()
-        w = np.random.rand(D)
-        V = np.random.rand(D, self.K)
+        w0 = 0
+        w = np.zeros(D)
+        V = np.zeros((D, self.K))
         self.coef = w0, w, V
         m_w0 = 0
         v_w0 = 0
@@ -80,14 +80,15 @@ class FactorizationMachines(BaseEstimator, RegressorMixin):
                     m_w0 = self.BETA1*m_w0 + (1-self.BETA1)*g_w0
                     v_w0 = self.BETA2*v_w0 + (1-self.BETA2)*np.square(g_w0)
                     w0 = w0 - self.ETA*(m_w0/(1-beta1t))/(np.sqrt(v_w0/(1-beta2t))+self.EPS)
-                    g_w = e*X[n] + r_w
-                    m_w = self.BETA1*m_w + (1-self.BETA1)*g_w
-                    v_w = self.BETA2*v_w + (1-self.BETA2)*np.square(g_w)
-                    w = w - self.ETA*(m_w/(1-beta1t))/(np.sqrt(v_w/(1-beta2t))+self.EPS)
-                    g_V = e*(X[n][:,np.newaxis]*np.dot(V.T, X[n]) - V*np.square(X[n][:,np.newaxis])) + r_V
-                    m_V = self.BETA1*m_V + (1-self.BETA1)*g_V
-                    v_V = self.BETA2*v_V + (1-self.BETA2)*np.square(g_V)
-                    V = V - self.ETA*(m_V/(1-beta1t))/(np.sqrt(v_V/(1-beta2t))+self.EPS)
+                    mask = np.where(X[n] != 0)[0]
+                    g_w = e*X[n][mask] + r_w[mask]
+                    m_w[mask] = self.BETA1*m_w[mask] + (1-self.BETA1)*g_w
+                    v_w[mask] = self.BETA2*v_w[mask] + (1-self.BETA2)*np.square(g_w)
+                    w[mask] = w[mask] - self.ETA*(m_w[mask]/(1-beta1t))/(np.sqrt(v_w[mask]/(1-beta2t))+self.EPS)
+                    g_V = e*(X[n][:,np.newaxis][mask]*np.dot(V[mask].T, X[n][mask]) - V[mask]*np.square(X[n][:,np.newaxis][mask])) + r_V[mask]
+                    m_V[mask] = self.BETA1*m_V[mask] + (1-self.BETA1)*g_V
+                    v_V[mask] = self.BETA2*v_V[mask] + (1-self.BETA2)*np.square(g_V)
+                    V[mask] = V[mask] - self.ETA*(m_V[mask]/(1-beta1t))/(np.sqrt(v_V[mask]/(1-beta2t))+self.EPS)
                 self.coef = w0, w, V
                 y_pred = np.array([self._predict(x, w0, w, V) for x in X])
                 error = mean_squared_error(y, y_pred)
@@ -136,9 +137,9 @@ class PropensityFactorizationMachines(FactorizationMachines):
         assert((0 <= p).all() and (p <= 1).all()) # p は確率
         X = X[:, :-1]
         N, D = X.shape
-        w0 = np.random.rand()
-        w = np.random.rand(D)
-        V = np.random.rand(D, self.K)
+        w0 = 0
+        w = np.zeros(D)
+        V = np.zeros((D, self.K))
         self.coef = w0, w, V
         m_w0 = 0
         v_w0 = 0
@@ -167,15 +168,15 @@ class PropensityFactorizationMachines(FactorizationMachines):
                     m_w0 = self.BETA1*m_w0 + (1-self.BETA1)*g_w0
                     v_w0 = self.BETA2*v_w0 + (1-self.BETA2)*np.square(g_w0)
                     w0 = w0 - self.ETA*(m_w0/(1-beta1t))/(np.sqrt(v_w0/(1-beta2t))+self.EPS)
-
-                    g_w = e*X[n] + r_w
-                    m_w = self.BETA1*m_w + (1-self.BETA1)*g_w
-                    v_w = self.BETA2*v_w + (1-self.BETA2)*np.square(g_w)
-                    w = w - self.ETA*(m_w/(1-beta1t))/(np.sqrt(v_w/(1-beta2t))+self.EPS)
-                    g_V = e*(X[n][:,np.newaxis]*np.dot(V.T, X[n]) - V*np.square(X[n][:,np.newaxis])) + r_V
-                    m_V = self.BETA1*m_V + (1-self.BETA1)*g_V
-                    v_V = self.BETA2*v_V + (1-self.BETA2)*np.square(g_V)
-                    V = V - self.ETA*(m_V/(1-beta1t))/(np.sqrt(v_V/(1-beta2t))+self.EPS)
+                    mask = np.where(X[n] != 0)[0]
+                    g_w = e*X[n][mask] + r_w[mask]
+                    m_w[mask] = self.BETA1*m_w[mask] + (1-self.BETA1)*g_w
+                    v_w[mask] = self.BETA2*v_w[mask] + (1-self.BETA2)*np.square(g_w)
+                    w[mask] = w[mask] - self.ETA*(m_w[mask]/(1-beta1t))/(np.sqrt(v_w[mask]/(1-beta2t))+self.EPS)
+                    g_V = e*(X[n][:,np.newaxis][mask]*np.dot(V[mask].T, X[n][mask]) - V[mask]*np.square(X[n][:,np.newaxis][mask])) + r_V[mask]
+                    m_V[mask] = self.BETA1*m_V[mask] + (1-self.BETA1)*g_V
+                    v_V[mask] = self.BETA2*v_V[mask] + (1-self.BETA2)*np.square(g_V)
+                    V[mask] = V[mask] - self.ETA*(m_V[mask]/(1-beta1t))/(np.sqrt(v_V[mask]/(1-beta2t))+self.EPS)
                 self.coef = w0, w, V
                 y_pred = np.array([self._predict(x, w0, w, V) for x in X])
                 error = mean_squared_error(y, y_pred)
@@ -252,9 +253,9 @@ class FactorizationMachinesLogisticRegression(BaseEstimator, ClassifierMixin):
         fit_start = time.time()
         saturation_counter = 0
         N, D = X.shape
-        w0 = np.random.rand()
-        w = np.random.rand(D)
-        V = np.random.rand(D, self.K)
+        w0 = 0
+        w = np.zeros(D)
+        V = np.zeros((D, self.K))
         self.coef = w0, w, V
         m_w0 = 0
         v_w0 = 0
@@ -282,14 +283,15 @@ class FactorizationMachinesLogisticRegression(BaseEstimator, ClassifierMixin):
                     m_w0 = self.BETA1*m_w0 + (1-self.BETA1)*g_w0
                     v_w0 = self.BETA2*v_w0 + (1-self.BETA2)*np.square(g_w0)
                     w0 = w0 - self.ETA*(m_w0/(1-beta1t))/(np.sqrt(v_w0/(1-beta2t))+self.EPS)
-                    g_w = e*X[n] + r_w
-                    m_w = self.BETA1*m_w + (1-self.BETA1)*g_w
-                    v_w = self.BETA2*v_w + (1-self.BETA2)*np.square(g_w)
-                    w = w - self.ETA*(m_w/(1-beta1t))/(np.sqrt(v_w/(1-beta2t))+self.EPS)
-                    g_V = e*(X[n][:,np.newaxis]*np.dot(V.T, X[n]) - V*np.square(X[n][:,np.newaxis])) + r_V
-                    m_V = self.BETA1*m_V + (1-self.BETA1)*g_V
-                    v_V = self.BETA2*v_V + (1-self.BETA2)*np.square(g_V)
-                    V = V - self.ETA*(m_V/(1-beta1t))/(np.sqrt(v_V/(1-beta2t))+self.EPS)
+                    mask = np.where(X[n] != 0)[0]
+                    g_w = e*X[n][mask] + r_w[mask]
+                    m_w[mask] = self.BETA1*m_w[mask] + (1-self.BETA1)*g_w
+                    v_w[mask] = self.BETA2*v_w[mask] + (1-self.BETA2)*np.square(g_w)
+                    w[mask] = w[mask] - self.ETA*(m_w[mask]/(1-beta1t))/(np.sqrt(v_w[mask]/(1-beta2t))+self.EPS)
+                    g_V = e*(X[n][:,np.newaxis][mask]*np.dot(V[mask].T, X[n][mask]) - V[mask]*np.square(X[n][:,np.newaxis][mask])) + r_V[mask]
+                    m_V[mask] = self.BETA1*m_V[mask] + (1-self.BETA1)*g_V
+                    v_V[mask] = self.BETA2*v_V[mask] + (1-self.BETA2)*np.square(g_V)
+                    V[mask] = V[mask] - self.ETA*(m_V[mask]/(1-beta1t))/(np.sqrt(v_V[mask]/(1-beta2t))+self.EPS)
                 self.coef = w0, w, V
                 y_pred = np.array([self._predict(x, w0, w, V) for x in X])
                 error = mean_squared_error(y, y_pred)
